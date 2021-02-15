0f6cf12c52eb5bb77d8c4e0399f162d66c04a754
Andrysha <chavkin94@mail.ru>
2021-02-15 11:25:41 +0900

    Ненмого поменяли

--- index.js
+++ index.js
@@ -15,3 +15,43 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+function App() {
+  const messages = ['hel1','hel2'];
+ 
+  const [posts, setPosts] = useState([]);
+  console.log(posts);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(json => { setPosts(json)})
+    }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Поймай кнопку.
+        </a>
+        {/* <h2> {messages.map((el, index, messages)=>(<div> {el} </div>)) 
+        } </h2> */}
+
+        <h2> {posts.map(item=>(<div> {[item.id,'. ' , item.title]} </div>)) 
+        } </h2>
+        <Button count="5" children1="Давай еще " enable children2="раз!!!" ></Button>
+        
+      </header>
+    </div>
+  );
+
+}
+
